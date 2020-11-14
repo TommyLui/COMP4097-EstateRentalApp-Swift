5ea5d8287e577e10fbc5572f951e91c9e42a8d3c
TommyLui <clp084013@gmail.com>
2020-11-14 05:03:49 -0800

    map page init

---
+++ RentalApp/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  RentalApp
+//
+//  Created by tommylui on 14/11/2020.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
